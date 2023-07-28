d96435b07177697233076dcbd6bebecb0d8e9331
João Victor <jvctor23@gmail.com>
2023-07-28 13:42:23 -0300

    jdbc 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import com.mysql.jdbc.Driver;
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
+
+    static String url = "jdbc:mysql://localhost:3306/?user=root";
+    static String user = "localhost";
+    static String password = "";
+    public static void main(String[] args) throws SQLException {
         System.out.println("Olá SERPRO");
+
+        Connection con = DriverManager.getConnection(url);
+
+
+
     }
 }
+
+
+
+
