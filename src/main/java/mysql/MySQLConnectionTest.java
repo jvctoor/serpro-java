6146c16c7cff63c9f9b25189eff6eb1dde2d830b
João Victor <jvctor23@gmail.com>
2023-08-01 23:21:18 -0300

    Testes CRUD

--- src/main/java/mysql/MySQLConnectionTest.java
+++ src/main/java/mysql/MySQLConnectionTest.java
@@ -2,16 +2,32 @@ package mysql;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
 import utils.HibernateUtil;
 
 import java.sql.SQLException;
 import java.util.List;
 
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MySQLConnectionTest {
+
+    // Anotações do JUnit
+    // @BeforeAll @BeforeEach
+    // @AfterAll @AfterEach
+
+    UsuarioDAO_JPA jpa_dao;
+
+    @Test
+    @BeforeAll
+    @DisplayName("Setup DAO")
+    void instanciarDAO() {
+        jpa_dao = new UsuarioDAO_JPA();
+        System.out.println("DAO Instanciado");
+    }
 
     @Test
     @DisplayName("Get all - Hibernate")
@@ -30,12 +46,23 @@ public class MySQLConnectionTest {
         Assertions.assertNotEquals(null, users);
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(longs = {1L, 2L, 3L, 4L, 5L})
     @DisplayName("Get by id - JPA")
-    void jpaGetUserById() throws Exception {
-        UsuarioDAO_JPA dao = new UsuarioDAO_JPA();
-        Usuario user = dao.getById(15L);
-        Assertions.assertEquals(15L, user.getId());
+    void jpaGetUserById(Long id) throws Exception {
+        Usuario user = jpa_dao.getById(id);
+        Assertions.assertEquals(id, user.getId());
     }
+
+    @RepeatedTest(value=2)
+    @DisplayName("Should not delete anyone if the id is null")
+    void shouldNotDeleteAnyoneIfIdIsNull() {
+        Assertions.assertThrows(Exception.class, () -> {
+            jpa_dao.removeById(null);
+        });
+    }
+
+
+
 
 }
