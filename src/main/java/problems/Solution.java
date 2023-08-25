d94b7afbe32e6c7b67717ea4df323d5486172b11
João Victor <jvctor23@gmail.com>
2023-08-24 21:11:27 -0300

    search insert

--- src/main/java/problems/Solution.java
+++ src/main/java/problems/Solution.java
@@ -254,5 +254,25 @@ class Solution {
         return true;
     }
 
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0 ; i < nums.length ; i++) {
+            if (i+1 > nums.length-1) {
+                if (target > nums[i]) {
+                    return nums.length;
+                }
+            }
+            if (nums[i] == target) {
+                return i;
+            }
+            else if (target > nums[i] && target < nums[i+1]) {
+                return i+1;
+            }
+            else if (target > nums[nums.length-1]) {
+                return nums.length;
+            }
+        }
+        return 0;
+    }
+
 
 }
